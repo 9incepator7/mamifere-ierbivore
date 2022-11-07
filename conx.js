766c8ed039764709dc70eae3422ff9e22355cee7
9incepator7 <9incepator7@9incepator7.com>
2022-11-07 18:53:56 +0100

    43rd

--- conx.js
+++ conx.js
@@ -1,1 +1,8 @@
-alert("ha!");
+var numarareIerbivore = 23;
+
+while (numarareIerbivore <= 47) {
+    console.log("inca nu sunt suficient de multe animale pe camp!");
+    numarareIerbivore++;
+}
+
+document.write("sunt mult prea multe animale pe camp!");
---
+++ ve2.js
@@ -0,0 +1,7 @@
+var dacaZebre = 17;
+
+if (dacaZebre < 30) {
+    document.write ("prea putine zebre in savana!");
+} else (dacaZebre > 50) {
+    document.write("prea multe zebre in savana!");
+}
