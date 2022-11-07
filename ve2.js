870b346fd1710da7f2c35f7947379d24f20bf9b2
9incepator7 <9incepator7@9incepator7.com>
2022-11-07 19:05:03 +0100

    44th

--- ve2.js
+++ ve2.js
@@ -2,6 +2,8 @@ var dacaZebre = 17;
 
 if (dacaZebre < 30) {
     document.write ("prea putine zebre in savana!");
-} else (dacaZebre > 50) {
-    document.write("prea multe zebre in savana!");
+} else if (dacaZebre > 50) {
+    document.write ("prea multe zebre in savana!");
+} else {
+    document.write ("suficiente zebre in savana!");
 }
